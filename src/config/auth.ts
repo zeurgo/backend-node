4b2ccba8601f38b0f5b1b6dda0f114a615fa871b
Walter <walter.zeurgo@gmail.com>
2020-10-14 22:11:11 -0300

    Upload de avatar, tratamento de erros

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '774114a3944d585e3088c5f9c942b001',
+    expiresIn: '1d',
+  },
+};
--- src/database/migrations/1601604576215-CreateAppointments.ts
+++ src/database/migrations/1601604576215-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1601604576215
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
